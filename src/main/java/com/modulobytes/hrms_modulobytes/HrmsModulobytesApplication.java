5e3233507ffbbcba89194f654b9f8a920538c583
dushyantpuri <dushyantpurigoswami67@gmail.com>
2024-12-14 16:03:01 +0530

    create

---
+++ src/main/java/com/modulobytes/hrms_modulobytes/HrmsModulobytesApplication.java
@@ -0,0 +1,13 @@
+package com.modulobytes.hrms_modulobytes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsModulobytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsModulobytesApplication.class, args);
+	}
+
+}
